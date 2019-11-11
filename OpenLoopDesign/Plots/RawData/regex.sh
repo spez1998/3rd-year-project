c75542f0da531067b71ccc26dda817c526a3ac5e
Sujit Malde <samman30@live.com>
2019-11-11 18:16:12 +0000

    Created BSD sed script to change LTspice default output format into csv

---
+++ OpenLoopDesign/Plots/RawData/regex.sh
@@ -0,0 +1,4 @@
+LC_ALL=C sed -i '' 's/[[:space:]]/,/g' dual_full_lt_pyle_notch_tl074.txt
+LC_ALL=C sed -i '' 's/[()]//g' dual_full_lt_pyle_notch_tl074.txt
+LC_ALL=C sed -i '' 's/[dB]//g' dual_full_lt_pyle_notch_tl074.txt
+LC_ALL=C sed -i '' 's/�//g' dual_full_lt_pyle_notch_tl074.txt
