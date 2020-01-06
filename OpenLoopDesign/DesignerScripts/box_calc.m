a156b3fd11ce29e7118b3ee92acd28b0f767755f
Sujit Malde <samman30@live.com>
2020-01-06 17:06:16 +0000

    Almost finished writing interim report.

--- OpenLoopDesign/DesignerScripts/box_calc.m
+++ OpenLoopDesign/DesignerScripts/box_calc.m
@@ -1,4 +1,7 @@
+%%%%%% USER EDITS GO HERE %%%%%%
+
 %%% SPECIFICATION BLOCK %%%
+% Add loudspeaker Thiele-Smalll parameters here
 Cms_spec = 0.00063
 Bl_spec = 6.16
 VAS_spec = 15.38
@@ -6,9 +9,18 @@ fs = 52.2
 Xmax = 0.004
 Mms = 0.0148
 Z_nom = 4
+speaker_face_diameter = 0.165 % Also known as Sd
 
-%%% ABOVE RESONANCE POWER REQUIREMENTS %%%
-f_above = 100
+% Select MDF thickness and define mounting face dimensions
+mdf_thickness = 0.012
+mounting_face_length_ext = 0.200
+mounting_face_width_ext = 0.200
+
+% Choose new resonant frequency
+f_above = 100 
+
+%%%%%% USER EDITS END HERE %%%%%%
+
 a_above = (2*pi*f_above)^2 * Xmax
 F_above = Mms * a_above
 Ip_above = F_above / Bl_spec
@@ -33,10 +45,6 @@ eight_ohm_poweramp_below = (((Ip)^2)/2) * 8
 
 %%% DIMENSIONS %%%
 int_vol = V_box / 1000
-mdf_thickness = 0.012
-speaker_face_diameter = 0.165
-mounting_face_length_ext = 0.200
-mounting_face_width_ext = 0.200
 box_depth_int = int_vol / ((mounting_face_length_ext-(2*mdf_thickness)) * (mounting_face_length_ext-(2*mdf_thickness)))
 box_depth_ext = (2*mdf_thickness) + box_depth_int
 
