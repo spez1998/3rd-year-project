9047148bdaea37d9242f186cdfc09f3d3cdbef2a
Sujit Malde <samman30@live.com>
2020-04-29 00:48:42 +0100

    Writing report

--- Design/OpenLoop/Simulations/Simulink/plotter.m
+++ Design/OpenLoop/Simulations/Simulink/plotter.m
@@ -1,3 +1,5 @@
+load('open_loop_sim_data.mat')
+
 % IEEE-friendly Appearance
 set(0,'defaultAxesFontName','Times New Roman');
 set(0,'defaultTextFontName','Times New Roman');
@@ -5,9 +7,25 @@ set(0,'defaultLegendFontName','Times New Roman');
 set(0,'defaultAxesGridLineStyle','-');
 set(0,'defaultAxesFontSize',14);
 
+% Time to frequency
+freq = 10:0.01899810019:200;
+freq = transpose(freq);
+
+open_um = [open_um freq]
+open_um_v_pks = findpeaks(open_um(:,8));
+open_um_pks = open_um(ismember([open_um(:,8)],open_um_v_pks),:);
+open_box_v_pks = findpeaks(open_box(:,8));
+open_box_pks = open_box(ismember([open_box(:,8)],open_box_v_pks),:);
+open_lt_v_pks = findpeaks(open_lt(:,8));
+open_lt_pks = open_lt(ismember([open_lt(:,8)],open_lt_v_pks),:);
+
 figure;
-plot(open_time,open(:,7),'Color','#964B00');
+plot(open_um_pks(:,10),open_um_pks(:,8));
 hold on;
-plot(open_box_time,open_box(:,7),'r');
-plot(lt_time,lt(:,7),'k');
+plot(open_box_pks(:,1),open_box_pks(:,8));
+plot(open_lt_pks(:,1),open_lt_pks(:,8));
 axis square;
+title('Compensator Comparison');
+xlabel('Time / s');
+ylabel('Voltage / V');
+legend({'PLPW6D Freestanding','PLPW6D in Box','PLPW6D, Box, Linkwitz Transform'},'Location','southeast');
